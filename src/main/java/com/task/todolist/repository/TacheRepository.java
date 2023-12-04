bb4ebc9c30227693c1fb36ae1b83abdde49f2c59
FerdaousseELB <f.el-baraka@univ-lyon2.fr>
2023-12-04 12:15:09 +0100

    Paggination & chercher un titre

--- src/main/java/com/task/todolist/repository/TacheRepository.java
+++ src/main/java/com/task/todolist/repository/TacheRepository.java
@@ -3,10 +3,17 @@ package com.task.todolist.repository;
 
 import com.task.todolist.entities.Tache;
 import com.task.todolist.entities.Utilisateur;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TacheRepository extends JpaRepository<Tache, Long> {
     List<Tache> findByUtilisateur(Utilisateur ferdaousse);
+    Page<Tache> findByTitreContains(String titre, Pageable pageable);
+    @Query("select t from Tache t where t.titre like :x")
+    Page<Tache> chercher(@Param("x") String titre, Pageable pageable);
 }
--- src/main/java/com/task/todolist/web/TacheControlleur.java
+++ src/main/java/com/task/todolist/web/TacheControlleur.java
@@ -4,9 +4,12 @@ import com.task.todolist.entities.Tache;
 import com.task.todolist.repository.TacheRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,13 @@ import java.util.List;
 public class TacheControlleur {
     private TacheRepository tacheRepository;
     @GetMapping("/index")
-    public String index(Model model){
-        List<Tache> tacheList= tacheRepository.findAll();
-        model.addAttribute("listTaches", tacheList);
+    public String index(Model model,@RequestParam(name="page", defaultValue = "0") int page,
+                                    @RequestParam(name="size", defaultValue = "5") int size,
+                                    @RequestParam(name ="titre", defaultValue = "") String kw){
+        Page<Tache> pagesTache= tacheRepository.findByTitreContains(kw,PageRequest.of(page,size));
+        model.addAttribute("listTaches", pagesTache.getContent());
+        model.addAttribute("pages", new int[pagesTache.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         return "taches";
     }
 
